212cda508954895593ac96ac4eae1a1a2cd93e65
leonardo <leonardofarena@gmail.com>
2024-10-25 13:20:57 -0300

    log1o

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,11 @@
 export const environment = {
     production: false,
    
-   base_url: 'http://localhost:3000/api'
+ //   base_url: 'http://localhost:3000/api'
      // base_url: 'https://apprizosfera-back-dev-adds.1.ie-1.fl0.io/api'
      // base_url: 'https://apprizosfera-back.vercel.app/api'
    
-     //base_url:  'https://apprizosfera-back-production.up.railway.app/api'
-  //  base_url:  'https://apprizosfera-back.onrender.com/api'
+    base_url:  'https://apprizosfera-back.onrender.com/api'
 
    
 
